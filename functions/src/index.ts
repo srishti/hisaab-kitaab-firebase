b9024d96cd0b9d64e4aec984ad3e42e46de36d07
Srishti Gupta <mail@srishtigupta.in>
2021-08-13 12:49:39 +0530

    adds rewrite rules

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,34 +7,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
-
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap: any, context: any) => {
-      // Grab the current value of what was written to Firestore.
-      const original = snap.data().original;
-
-      // Access the parameter `{documentId}` with `context.params`
-      functions.logger.log('Uppercasing', context.params.documentId, original);
-      
-      const uppercase = original.toUpperCase();
-      
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to Firestore.
-      // Setting an 'uppercase' field in Firestore document returns a Promise.
-      return snap.ref.set({uppercase}, {merge: true});
-    });
+const express = require('express');
+const app = express();
 
 enum AccountType {
     Cash = "cash",
@@ -42,30 +16,11 @@ enum AccountType {
     Wallet = "wallet"
 }
 
-// interface Account {
-//     name: string;
-//     type: AccountType;
-//     currentBalance: number;
-// }
-
 function checkNullOrUndefined(obj: any) {
     return obj === null || obj === undefined
 }
 
-exports.addAccount = functions.https.onRequest(async (req: any, res: any) => {
-    const data = req.body;
-
-    // check name, type and currentBalance exists and is not empty
-    if (!checkNullOrUndefined(data.name) && !checkNullOrUndefined(data.type) && !checkNullOrUndefined(data.currentBalance) && typeof data.name == "string" && typeof data.type == "string" && typeof data.currentBalance === "number" && Object.values(AccountType).includes(data.type)) {
-        let id = uuidv4();
-        await admin.firestore().collection('accounts').doc(id).set({id: id, name: data.name, type: data.type, currentBalance: data.currentBalance});
-        res.json({id: id, name: data.name, type: data.type, currentBalance: data.currentBalance})
-    } else {
-        res.status(400).json({success: false})
-    }
-});
-
-exports.getAccounts = functions.https.onRequest(async (req: any, res: any) => {
+app.get('/accounts', async (req: any, res: any) => {
     admin.firestore().collection("accounts")
     .get()
     .then((querySnapshot: any) => {
@@ -80,7 +35,35 @@ exports.getAccounts = functions.https.onRequest(async (req: any, res: any) => {
     });
 });
 
-exports.addTransaction = functions.https.onRequest(async (req: any, res: any) => {
+app.post('/accounts/add', async (req: any, res: any) => {
+    const data = req.body;
+
+    // check name, type and currentBalance exists and is not empty
+    if (!checkNullOrUndefined(data.name) && !checkNullOrUndefined(data.type) && !checkNullOrUndefined(data.currentBalance) && typeof data.name == "string" && typeof data.type == "string" && typeof data.currentBalance === "number" && Object.values(AccountType).includes(data.type)) {
+        let id = uuidv4();
+        await admin.firestore().collection('accounts').doc(id).set({id: id, name: data.name, type: data.type, currentBalance: data.currentBalance});
+        res.json({id: id, name: data.name, type: data.type, currentBalance: data.currentBalance})
+    } else {
+        res.status(400).json({success: false})
+    }
+});
+
+app.get('/transactions', async (req: any, res: any) => {
+    admin.firestore().collection("transactions")
+    .get()
+    .then((querySnapshot: any) => {
+        let result: any[] = [];
+        querySnapshot.forEach((doc: any) => {
+            result.push(doc.data());
+        });
+        res.json(result);
+    })
+    .catch((error: any) => {
+        res.status(400).json({success: false});
+    });
+});
+
+app.post('/transactions/add', async (req: any, res: any) => {
     const data = req.body;
     //date
     //description
@@ -116,17 +99,4 @@ exports.addTransaction = functions.https.onRequest(async (req: any, res: any) =>
     }
 });
 
-exports.getTransactions = functions.https.onRequest(async (req: any, res: any) => {
-    admin.firestore().collection("transactions")
-    .get()
-    .then((querySnapshot: any) => {
-        let result: any[] = [];
-        querySnapshot.forEach((doc: any) => {
-            result.push(doc.data());
-        });
-        res.json(result);
-    })
-    .catch((error: any) => {
-        res.status(400).json({success: false});
-    });
-});
+exports.api = functions.https.onRequest(app);
